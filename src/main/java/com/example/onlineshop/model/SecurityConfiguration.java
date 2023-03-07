91038abc66be3c839a3819e9666b94da6200c51d
Mastani-ep <eparuch@student.agh.edu.pl>
2023-03-07 05:38:36 +0100

    Working registration - login attempt

--- src/main/java/com/example/onlineshop/model/SecurityConfiguration.java
+++ src/main/java/com/example/onlineshop/model/SecurityConfiguration.java
@@ -1,21 +1,17 @@
 package com.example.onlineshop.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static java.lang.String.format;
@@ -23,6 +19,34 @@ import static java.lang.String.format;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Resource(name = "customUserService")
+    private CustomUserDetailService userDetailsService;
+
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .build();
+    }
+
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authProvider());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(5);
+    }
 
     public static final String[] ENDPOINTS_WHITELIST = {
             "/css/**",
@@ -37,24 +61,12 @@ public class SecurityConfiguration {
     public static final String LOGIN_FAIL_URL = LOGIN_URL + "?error";
     public static final String DEFAULT_SUCCESS_URL = "/home";
 
-    private final UserRepository userRepository;
-
-    public SecurityConfiguration(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .build();
-    }
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(request ->
                         request.requestMatchers(ENDPOINTS_WHITELIST).permitAll()
-                        .requestMatchers("/hello").authenticated())
+                                .requestMatchers("/hello").authenticated())
                 .csrf().disable()
                 .formLogin(form -> form
                         .loginPage(LOGIN_URL)
@@ -62,12 +74,12 @@ public class SecurityConfiguration {
                         .failureUrl(LOGIN_FAIL_URL)
                         .defaultSuccessUrl(DEFAULT_SUCCESS_URL))
                 .logout(logout -> logout
-                    .logoutUrl("/logout")
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID")
-                    .logoutSuccessUrl(LOGIN_URL + "?logout"));
+                        .logoutUrl("/logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .logoutSuccessUrl(LOGIN_URL + "?logout"));
         return http.build();
     }
-    
+
 
 }
--- src/main/java/com/example/onlineshop/model/UserRepository.java
+++ src/main/java/com/example/onlineshop/model/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.onlineshop.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
